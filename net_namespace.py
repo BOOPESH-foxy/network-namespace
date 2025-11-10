c99310b2ed7886bf71603c6493e7e4817624914d
boopesh-foxy <boopesh.mc@gmail.com>
2025-11-10 16:38:43 +0530

    repo init

---
+++ net_namespace.py
@@ -0,0 +1,18 @@
+from pyroute2 import IPRoute, NetNS, netns
+from ipaddress import IPv4Network
+
+
+class NamespaceManager:
+    def __init__(self):
+        pass
+
+    def create_namespace():
+        pass
+
+    def delete_namespace():
+        pass
+
+    def create_veth_pair():
+        pass
+
+    
